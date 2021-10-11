583400138a2f157ee3168c9ab009e21d24214d48
Abbas Hussain <abbas_10690@yahoo.com>
2021-10-11 02:36:51 -0700

    Add new monitoring code
    Signed-off-by: Abbas Hussain <abbas_10690@yahoo.com>

--- cdk/opensearch-website-search/lambdas/monitoring-lambda/monitor.py
+++ cdk/opensearch-website-search/lambdas/monitoring-lambda/monitor.py
@@ -45,6 +45,11 @@ MetricData=[
 """
 
 
+def to_upper_camel_case(snake_str):
+  components = snake_str.split('_')
+  return components[0] + ''.join(x.title() for x in components[1:])
+
+
 class CWMetricData():
 
   def __init__(self, namespace):
@@ -62,26 +67,102 @@ class CWMetricData():
 
 
 def check_cluster_health(metric_data):
-  # TODO: whether cluster is RED, YELLOW or GREEN, this is not bullet proof code
   cluster_health_url = opensearch_base_url + '/_cluster/health?pretty'
-  # ES 6.x requires an explicit Content-Type header
   headers = {"Content-Type": "application/json"}
 
-  val = 0
+  STATUS_PREFIX = 'ClusterStatus.'
+  SHARD_PREFIX = 'Shards.'
+  COLORS = ["green", "red", "yellow"]
+  SHARD_COUNT_METRICS = ["active_primary_shards", "active_shards",
+                         "delayed_unassigned_shards", "initializing_shards",
+                         "relocating_shards", "unassigned_shards"]
   try:
     res = requests.get(cluster_health_url, auth=http_basic_auth, headers=headers, verify=False)
     health = res.json()
-    if health['status'] == 'green':
-      val = 1
+
+    # check master available metric
+    if "discovered_master" in health and health["discovered_master"]:
+      metric_data.add({
+        'MetricName': 'MasterReachableFromNLB',
+        'Value': 1.0,
+        'Unit': 'Count',
+        'StorageResolution': 60
+      })
+    else:
+      metric_data.add({
+        'MetricName': 'MasterReachableFromNLB',
+        'Value': 0.0,
+        'Unit': 'Count',
+        'StorageResolution': 60
+      })
+    print("Successfully collected master metrics")
+
+    # cluster health status metric
+    if "status" in health:
+      for color in COLORS:
+        if health['status'] == color:
+          metric_data.add({
+            'MetricName': STATUS_PREFIX + color,
+            'Value': 1.0,
+            'Unit': 'Count',
+            'StorageResolution': 60
+          })
+        else:
+          metric_data.add({
+            'MetricName': STATUS_PREFIX + color,
+            'Value': 0.0,
+            'Unit': 'Count',
+            'StorageResolution': 60
+          })
+    print("Successfully emitted cluster status metrics")
+
+    # Add node count metric
+    if "number_of_nodes" in health:
+      metric_data.add({
+        'MetricName': 'Nodes',
+        'Value': health["number_of_nodes"],
+        'Unit': 'Count',
+        'StorageResolution': 60
+      })
+    print("Successfully collected node count metric")
+
+    # Add shard count metric
+    for shard_type in SHARD_COUNT_METRICS:
+      if shard_type in health:
+        metric_data.add({
+          'MetricName': SHARD_PREFIX + to_upper_camel_case(shard_type.removeprefix("_shards")),
+          'Value': health[shard_type],
+          'Unit': 'Count',
+          'StorageResolution': 60
+        })
+    print("Successfully collected shard count metrics")
+
+    # Add pending task metric
+    if "number_of_pending_tasks" in health:
+      metric_data.add({
+        'MetricName': 'PendingTasks',
+        'Value': health["number_of_pending_tasks"],
+        'Unit': 'Count',
+        'StorageResolution': 60
+      })
+    print("Successfully collected pending_tasks metrics")
+
+    metric_data.add({
+      'MetricName': 'ClusterHealth.Failed',
+      'Value': 0.0,
+      'Unit': 'Count',
+      'StorageResolution': 60
+    })
+    print("All check_cluster_health metrics collected")
+
   except Exception as e:
     print(e)
-
-  metric_data.add({
-    'MetricName': 'ClusterHealth',
-    'Value': val,
-    'Unit': 'Count',
-    'StorageResolution': 60
-  })
+    metric_data.add({
+      'MetricName': 'ClusterHealth.Failed',
+      'Value': 1.0,
+      'Unit': 'Count',
+      'StorageResolution': 60
+    })
 
 
 def handler(event, context):
@@ -90,5 +171,3 @@ def handler(event, context):
   check_cluster_health(metrics)
   cw.put_metric_data(Namespace=metrics.get_namespace(),
                      MetricData=metrics.get_all_metrics())
-
-
