4431d4aaae2d84252df2ad380de8b69090abe579
Abbas Hussain <abbas_10690@yahoo.com>
2021-11-07 16:46:25 -0800

    Fix dashboard installation
    Signed-off-by: Abbas Hussain <abbas_10690@yahoo.com>

--- cdk/opensearch-website-search/userdata/tar/dashboards.sh
+++ cdk/opensearch-website-search/userdata/tar/dashboards.sh
@@ -1,9 +1,11 @@
-#Installing Oopensearch dashboards
+#Installing Opensearch dashboards
 dashboards_url="${__DASHBOARDS_URL__}"
 cd /
 curl "${!dashboards_url}" -o opensearch-dashboards
 tar zxf opensearch-dashboards
 chown -R ec2-user:ec2-user opensearch-dashboards-*
-cd opensearch-dashboards-*
+cd opensearch-dashboards-* || exit
+sudo sed -i /opensearch.hosts/s/https/http/1 config/opensearch_dashboards.yml
 echo "server.host: 0.0.0.0" >> config/opensearch_dashboards.yml
+sudo -u ec2-user touch dashboard_install.log
 sudo -u ec2-user nohup ./bin/opensearch-dashboards > dashboard_install.log 2>&1 &
